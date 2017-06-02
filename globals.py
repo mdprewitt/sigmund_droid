435a30bf3a7fa41ed918cf132d65177d5431b7a2
Marc Prewitt <mdprewitt@users.noreply.github.com>
2017-06-02 12:53:23 -0400

    add globals for direction

--- globals.py
+++ globals.py
@@ -3,3 +3,17 @@ Y_POSITION = 0
 
 RED_DESK = tuple(30, 20)
 BLUE_DESK = tuple(30, -20)
+
+ROTATIONS_PER_CM = .7
+ROTATIONS_PER_DEGREE = .5  # how many rotations to turn specified degrees
+"""
+           0
+           |
+-90/270 ---|--- 90/-270
+           |
+       180/-180
+"""
+DIRECTION = 0 
+
+LEFT_MOTOR = LargeMotor(OUTPUT_A)
+RIGHT_MOTOR = LargeMotor(OUTPUT_B)
